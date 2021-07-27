59ac1f35329e2d49d73980d4e581a1aa886820dd
sembianbalasubramanian <sembianb@gmail.com>
2021-07-27 07:22:29 -0500

    Initial Version

---
+++ main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+#TODO: REFERENCES
+#https://theappsolutions.com/blog/development/app-like-yelp-development/
+#https://www.kaggle.com/fahd09/yelp-dataset-surpriseme-recommendation-system
+#https://towardsdatascience.com/yelp-restaurant-recommendation-system-capstone-project-264fe7a7dea1
+#https://medium.com/web-mining-is688-spring-2021/yelp-restaurant-rating-prediction-and-recommendation-system-fffd26fcb57a
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
